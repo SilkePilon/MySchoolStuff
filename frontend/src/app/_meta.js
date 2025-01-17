924651fbbdc83c8dc738f7251d640b739f67cd9e
Silke pilon <silkepilon2009@gmail.com>
2025-01-17 14:19:08 +0100

    init

---
+++ frontend/src/app/_meta.js
@@ -0,0 +1,13 @@
+export default {
+  index: {
+    display: "hidden",
+  },
+  docs: {
+    type: "page",
+    title: "Opdrachten",
+  },
+  stage: {
+    type: "page",
+    title: "Stage",
+  },
+};
